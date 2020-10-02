90b6b669fbf69a0f66f871ddbf3937e4a56fbba9
Vuli Russell <vulirussell@gmail.com>
2020-10-02 10:53:28 +0100

    added styling

--- main.js
+++ main.js
@@ -1,88 +1,96 @@
 const morseEnglishDictionay ={
-    'A':'• −',
-    'B':'− • • •',
-    'C':'− • − •',	
-    'D':'− • •',
-    'E':'•',
-    'F':'• • − •',	
-    'G':'− − •',	
-    'H':'• • • •',	
-    'I':'• •',	
-    'J':'• − − −',	
-    'K':'− • −',	
-    'L':'• − • •',
-    'M':'− −',
+    'A':'. -',
+    'B':'- . . .',
+    'C':'- . - .',	
+    'D':'- . .',
+    'E':'.',
+    'F':'. . - .',	
+    'G':'- - .',	
+    'H':'. . . .',	
+    'I':'. .',	
+    'J':'. - - -',	
+    'K':'- . -',	
+    'L':'. - . .',
+    'M':'- -',
 
-    'N':'− •',
-    'O':'− − −',
-    'P':'• − − •',
-    'Q':'− − • −',
-    'R':'• − •',
-    'S':'• • •',
-    'T':'−',
-    'U':'• • −',
-    'V':'• • • −',
-    'W':'• − −',
-    'X':'− • • −',
-    'Y':'− • − −',
-    'Z':'− − • •',
+    'N':'- .',
+    'O':'- - -',
+    'P':'. - - .',
+    'Q':'- - . -',
+    'R':'. - .',
+    'S':'. . .',
+    'T':'-',
+    'U':'. . -',
+    'V':'. . . -',
+    'W':'. - -',
+    'X':'- . . -',
+    'Y':'- . - -',
+    'Z':'- - . .',
 
-    'Ä':'• − • −',
-    'Á':'• − − • −',
-    'Å':'• − − • −',
-    'É':'• • − • •',
-    'Ñ':'− − • − −',
-    'Ö':'− − − •',
-    'Ü':'• • − −',
+    'Ä':'. - . -',
+    'Á':'. - - . -',
+    'Å':'. - - . -',
+    'É':'. . - . .',
+    'Ñ':'- - . - -',
+    'Ö':'- - - .',
+    'Ü':'. . - -',
 
-    '0':'− − − − −',	
-    '1':'• − − − −',	
-    '2':'• • − − −',	
-    '3':'• • • − −',	
-    '4':'• • • • −',	
-    '5':'• • • • •',	
-    '6':'− • • • •',	
-    '7':'− − • • •',	
-    '8':'− − − • •',	
-    '9':'− − − − •',
+    '0':'- - - - -',	
+    '1':'. - - - -',	
+    '2':'. . - - -',	
+    '3':'. . . - -',	
+    '4':'. . . . -',	
+    '5':'. . . . .',	
+    '6':'- . . . .',	
+    '7':'- - . . .',	
+    '8':'- - - . .',	
+    '9':'- - - - .',
 
-    '.':'• − • − • −',
-    ',':'− − • • − −',
-    ':':'− − − • • •',
-    '?':'• • − − • •',
-    '\'':'• − − − − •',
-    '-':'− • • • • −',
-    '\/':'− • • − •',
-    '\(':'− • − − • −',
-    '\)':'− • − − •',
-    "\"":'• − • • − •',
-    '@':'• − − • − •',
-    '=':'− • • • −',
+    '.':'. - . - . -',
+    ',':'- - . . - -',
+    ':':'- - - . . .',
+    '?':'. . - - . .',
+    '\'':'. - - - - .',
+    '-':'- . . . . -',
+    '\/':'- . . - .',
+    '\(':'- . - - . -',
+    '\)':'- . - - .',
+    "\"":'. - . . - .',
+    '@':'. - - . - .',
+    '=':'- . . . -',
 
     '':''
 }
 
 
-const characterToMorse = char => morseEnglishDictionay[char.toUpperCase()];
 
-
-const wordToMorse = word => {
-    let letters = word.split('');
-    let morseLetters = letters.map(letter => characterToMorse(letter))
-    return morseLetters.join('   ');
-};
-
-const sentenceToMorse = sentence => {
+const latinToMorse = sentence => {
     let words = sentence.split(' ');
-    let morseWords = words.map(word => wordToMorse(word))
+    let morseWords = words.map(word => {
+        let letters = word.split('');
+        let morseLetters = letters.map(letter => morseEnglishDictionay[letter.toUpperCase()]);
+        return morseLetters.join('   ');
+    });
     return morseWords.join('       ');
 };
+
+const morseToLatin = sentence => {
+    let words = sentence.split('       ');
+    let latinWords = words.map(word => {
+        let letters = word.split('   ');
+        let latinLetters = letters.map(letter => Object.keys(morseEnglishDictionay).find(key => morseEnglishDictionay[key]===letter));
+        return latinLetters.join('');
+    });
+    return latinWords.join(' ');
+
+}
 
 latinText = document.getElementById('latin-chars');
 morseCode = document.getElementById('morse-code');
 
-document.querySelector('button').addEventListener('click',()=>{
-    morseCode.value = sentenceToMorse(latinText.value);
-})
+
+latinText.addEventListener('input',()=>{morseCode.value = latinToMorse(latinText.value)});
+morseCode.addEventListener('input',()=>{latinText.value = morseToLatin(morseCode.value)});
+
 
 
